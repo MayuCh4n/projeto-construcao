b43f4647e56a377e7c5184d21794fa3ea02a79aa
Victor Hugo da Silva <victorhjoe@yahoo.com.br>
2020-07-23 12:49:59 -0300

    ajuste para telas xl

--- src/components/páginas/Servicios.vue
+++ src/components/páginas/Servicios.vue
@@ -8,14 +8,14 @@
         </p>
       </b-col>
 
-      <b-col cols="3" md="3" sm="12" xs="12">
-          <b-col md="3" sm="12" xs="12">
+      <b-col cols="3" xl="3" md="3" sm="12" xs="12">
+          <b-col xl="12" md="3" sm="12" xs="12">
             <img src="../../assets/servicios-imgs/braço-carregamento.png" id="imgbraço">  
           </b-col>
       </b-col>
 
-      <b-col cols="3" md="3" sm="12" xs="12">
-          <b-col md="12" sm="12" xs="12">
+      <b-col cols="3" xl="3" md="3" sm="12" xs="12" class="link">
+          <b-col xl="12" md="12" sm="12" xs="12">
             informações tecnicas <br>
             clique
             <a href="http://" target="_blank" rel="noopener noreferrer">aqui</a>
@@ -59,6 +59,11 @@ export default {
 
     padding: 15px;
     margin-top: 10px;
+  }
+
+  .link {
+    text-align: justify;
+    margin-top: 100px;
   }
 
 </style>
