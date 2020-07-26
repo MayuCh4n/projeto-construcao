6e2e32545b04149b34275f2336a7427e44d61635
MayuCh4n <mayumimatsumoto@outlook.com>
2020-07-26 13:11:37 -0300

    ajuste página de contato

--- src/components/páginas/Contacto.vue
+++ src/components/páginas/Contacto.vue
@@ -1,5 +1,6 @@
 <template>
   <b-container class="bv-example-row">
+
     <b-row align-h="center">
         <b-col  md="6" class="nome">
           <p class="text2"> Para dúvidas, contacte-nos:</p>
@@ -7,21 +8,43 @@
             <b-form-input id="nome"  v-model="text" placeholder="Enter your name"></b-form-input>
           </b-form-group>
         </b-col>
-        
     </b-row>
+
+    <b-row align-h="center">
         <b-col  md="6" class="email">
            <b-form-group label="Email:" label-cols-md="2" label-for="email" label-align="left">
-            <b-form-input id="email"  v-model="text" placeholder="Enter your email"></b-form-input>
+            <b-form-input id="email"  v-model="email" placeholder="Enter your email"></b-form-input>
           </b-form-group>
         </b-col>
-  </b-container>
+    </b-row>
+
+    <b-row align-h="center">
+        <b-col  md="6" class="tel">
+          <b-form-group label="Telefone:" label-cols-md="2" label-for="tel" label-align="left">
+            <b-form-input id="tel" type="number" v-model="tel" placeholder="Enter your tel"></b-form-input>
+          </b-form-group>
+        </b-col>
+    </b-row>
+
+    <b-row align-h="center">
+        <b-col  md="6" class="textarea">
+          <b-form-group label="Mensagem:" label-cols-md="2" label-for="tel" label-align="left">
+            <b-form-textarea id="textarea" v-model="mensagem" placeholder="Enter your message"></b-form-textarea>
+          </b-form-group>
+        </b-col>
+    </b-row>
+    
+    <iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26631.154489424327!2d-70.6760310158102!3d-33.45205996297091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5061664cc23%3A0x7c797ae247d38105!2sSantiago%2C%20Chile!5e0!3m2!1spt-BR!2sbr!4v1595779447201!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </b-container>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        text: ''
+        text: '',
+        email: '',
+        tel: ''
       }
     }
   }
@@ -32,12 +55,31 @@
     margin-top: 30px;
   }
   .email{
-    margin-top: 10px;
-    margin-left: 270px;
+    margin-top: 20px;
+  }
+  .tel{
+    margin-top: 20px;
+  }
+  .textarea{
+    margin-top: 20px;
+    margin-bottom: 30px;
   }
   .text2{
     text-align: center;
     font-size:20px;
     padding: 0;
+  }
+  .mapa{
+    margin-bottom: 30px;
+  }
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
   }
 </style>
